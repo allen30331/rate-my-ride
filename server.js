b07b77164e15a72b54e5b18fbd2e9123f0135bd1
allen30331 <allen30331@gmail.com>
2017-02-07 12:02:26 -0500

    first push

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+//app.listen(process.env.PORT || 8080);
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+//runServer();
+
+
+module.exports = {app, runServer, closeServer};
+
---
+++ test/test-root.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const should = require('chai').should();
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const {app, runServer, closeServer} = require('../server');
+
+
+describe('access folder', function() {
+
+ before(function() {
+    return runServer();
+  });
+
+  afterEach(function() {
+    return closeServer();
+  })
+
+
+  describe('access root folder', function() {
+  	it('should return 200 status code and html', function() {
+  	let res;
+	return chai.request(app)
+		.get('/')
+		.then(function(_res) {
+			res = _res;
+			res.should.have.status(200);
+			res.should.be.html;
+  	});
+	
+	});	
+  });		
+});
+
