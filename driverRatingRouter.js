25002a79027e88d231f505a6ba72d6cde419a1e3
allen30331 <allen30331@gmail.com>
2017-02-09 11:13:49 -0500

    initial commit

---
+++ driverRatingRouter.js
@@ -0,0 +1,12 @@
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find()
+    .exec()
+    .then(posts => {
+      res.json(posts.map(post => post.apiRepr()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'something went terribly wrong'});
+    });
+});
---
+++ models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const rateDriverSchema = mongoose.Schema({
+  driverName: {type: String, required: true},
+  company: {type: String, required: true},
+  tagNumber: {type: Number, required: true},
+  driverRating: {type: Number, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+
+// rateDriverSchema.virtual('authorName').get(function() {
+//   return `${this.author.firstName} ${this.author.lastName}`.trim();
+// });
+
+rateDriverSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    author: this.authorName,
+    content: this.content,
+    title: this.title,
+    created: this.created
+  };
+}
+
+const RateDriver = mongoose.model('RateDriver', rateDriverSchema);
+
+module.exports = {RateDriver};
