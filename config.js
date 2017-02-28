ad676519dd1ae8ece2eda646b9991191dc980205
allen30331 <allen30331@gmail.com>
2017-02-27 20:39:09 -0500

    changed config file

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       // global.DATABASE_URL ||
-                      'mongodb://localhost:27017/rate-my-ride';
+                       global.DATABASE_URL ||
+                      'mongodb://localhost/rate-my-ride';
 exports.PORT = process.env.PORT || 8080;
+
+
+
+
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                        // global.DATABASE_URL ||
+//                       'mongodb://localhost:27017/rate-my-ride';
+// exports.PORT = process.env.PORT || 8080;
